714ba4b923facf63bf16493911fa49bf56e4d8d4
Luciano Pennacchioni <lucianopennacchioni@roagro.com.ar>
2025-05-22 20:58:10 -0300

    fix: :bug: add missing decorator in account service

--- src/account/account.service.ts
+++ src/account/account.service.ts
@@ -1,10 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Account } from './entities/account.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class AccountService {
-    constructor(private readonly accountRepository: Repository<Account>) { }
+    constructor(
+        @InjectRepository(Account)
+        private readonly accountRepository: Repository<Account>
+    ) { }
 
     async findOne(address: string) {
         const account = await this.accountRepository.findOne({ where: { address } })
