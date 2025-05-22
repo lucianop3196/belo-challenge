c6d6ee03d67cf318b784ed0f1fc975c0e78dee4f
Luciano Pennacchioni <lucianopennacchioni@roagro.com.ar>
2025-05-22 19:02:06 -0300

    feat: :sparkles: create public decorator

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getAppVersion(): string {
+    const pjson = require('../package.json');
+
+    return `Version api ${pjson.version}`;
   }
 }
--- src/guards/api-key.guard.ts
+++ src/guards/api-key.guard.ts
@@ -1,25 +1,35 @@
 
 import { Injectable, CanActivate, ExecutionContext, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC_KEY } from 'src/decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
-    constructor(private readonly configService: ConfigService) {}
+    constructor(private readonly configService: ConfigService, private readonly reflector: Reflector) { }
 
 
-  canActivate(
-    context: ExecutionContext,
-  ): boolean | Promise<boolean> | Observable<boolean> {
-    const request = context.switchToHttp().getRequest();
+    canActivate(
+        context: ExecutionContext,
+    ): boolean | Promise<boolean> | Observable<boolean> {
+        const request = context.switchToHttp().getRequest();
+        const isPublic = this.reflector.getAllAndOverride<boolean>(IS_PUBLIC_KEY, [
+            context.getHandler(),
+            context.getClass(),
+        ]);
 
-    const apiKey = request.headers['x-api-key'];
-    const validApiKey = this.configService.get<string>('API_KEY');
+        if (isPublic) {
+            return true;
+        }
 
-    if (apiKey !== validApiKey) {
-      throw new UnauthorizedException('Debes prooveer una API key válida');
+        const apiKey = request.headers['x-api-key'];
+        const validApiKey = this.configService.get<string>('API_KEY');
+
+        if (apiKey !== validApiKey) {
+            throw new UnauthorizedException('Debes prooveer una API key válida');
+        }
+
+        return true
     }
-
-    return true
-  }
 }
